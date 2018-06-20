3e972a42856db9b30085a256490b02c566c49f23
Adam Radocz <adam.radocz@gmail.com>
2018-06-20 20:03:23 +0000

    Removed the /dev/input/mice workaround.

--- bin/start-boinc.sh
+++ bin/start-boinc.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-# Workaround for activity checking which sends "Could not open directory '/dev/input/mice' from '/var/lib/boinc-client'" errors
-mkdir -p /dev/input/mice
-
 # Configure the GUI RPC
 echo $BOINC_GUI_RPC_PASSWORD > /var/lib/boinc-client/gui_rpc_auth.cfg
 echo $BOINC_REMOTE_HOST > /var/lib/boinc-client/remote_hosts.cfg
