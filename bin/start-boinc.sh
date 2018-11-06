2a49ec98a3eec4549012c63030d4d13e77a27706
Adam Radocz <adam.radocz@gmail.com>
2018-11-06 16:42:12 +0100

    Modified the start-boinc.sh too

--- bin/start-boinc.sh
+++ bin/start-boinc.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Configure the GUI RPC
-echo $BOINC_GUI_RPC_PASSWORD > /var/lib/boinc-client/gui_rpc_auth.cfg
-echo $BOINC_REMOTE_HOST > /var/lib/boinc-client/remote_hosts.cfg
+echo $BOINC_GUI_RPC_PASSWORD > /var/lib/boinc/gui_rpc_auth.cfg
+echo $BOINC_REMOTE_HOST > /var/lib/boinc/remote_hosts.cfg
 
 # Run BOINC. Full path needs for GPU support.
 exec /usr/bin/boinc $BOINC_CMD_LINE_OPTIONS
