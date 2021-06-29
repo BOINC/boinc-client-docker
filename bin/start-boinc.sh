83f12667914e4547e644dc854175e29239b059ff
choelzl <cedric.hoelzl@epfl.ch>
2021-06-30 00:04:32 +0200

    Switch to boinccmd
    We are unable to pass arguments using the envar without encoutering errors. To circumvent this, running boinccmd instead of boinc seems to be the solution.
    Logs:
    The command-line options for /usr/bin/boinc are intended for debugging.
    The recommended command-line interface is a separate program,'boinccmd'.
    Run boinccmd in the same directory as /usr/bin/boinc.

--- bin/start-boinc.sh
+++ bin/start-boinc.sh
@@ -5,4 +5,4 @@ echo $BOINC_GUI_RPC_PASSWORD > /var/lib/boinc/gui_rpc_auth.cfg
 echo $BOINC_REMOTE_HOST > /var/lib/boinc/remote_hosts.cfg
 
 # Run BOINC. Full path needs for GPU support.
-exec /usr/bin/boinc $BOINC_CMD_LINE_OPTIONS
+exec /usr/bin/boinccmd $BOINC_CMD_LINE_OPTIONS
