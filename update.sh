7edd1d5ec99178e5833f6686ce5ab2b4807699cb
Adam Radocz <adam.radocz@gmail.com>
2021-08-20 19:35:59 +0200

    Add update script to build and push images to Docker Hub.

---
+++ update.sh
@@ -0,0 +1,31 @@
+# Ubuntu Base
+docker build -t boinc/client:base-ubuntu -f Dockerfile.base-ubuntu .
+docker push boinc/client:base-ubuntu
+
+# Alpine Base
+docker build -t boinc/client:base-alpine -f Dockerfile.base-alpine .
+docker push boinc/client:base-alpine
+
+# Nvidia
+docker build -t boinc/client:nvidia -f Dockerfile.nvidia .
+docker push boinc/client:nvidia
+
+# AMD
+docker build -t boinc/client:amd -f Dockerfile.amd .
+docker push boinc/client:amd
+
+# Intel
+docker build -t boinc/client:intel -f Dockerfile.intel .
+docker push boinc/client:intel
+
+# Intel Legacy
+docker build -t boinc/client:intel-legacy -f Dockerfile.intel-legacy .
+docker push boinc/client:intel-legacy
+
+# Multi GPU
+docker build -t boinc/client:multi-gpu -f Dockerfile.multi-gpu .
+docker push boinc/client:multi-gpu
+
+# VirtualBox
+docker build -t boinc/client:virtualbox -f Dockerfile.virtualbox .
+docker push boinc/client:virtualbox
