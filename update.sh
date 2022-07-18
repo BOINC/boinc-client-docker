a66484d7b0d281df67c9145c9476a00fe486c69b
d2r4 <d2r4@hotmail.com>
2022-07-18 18:22:29 -0400

    enable local arm builds

--- update.sh
+++ update.sh
@@ -30,5 +30,16 @@ docker push boinc/client:multi-gpu
 docker build -t boinc/client:virtualbox -f Dockerfile.virtualbox .
 docker push boinc/client:virtualbox
 
+# Enable builds for other architectures (i.e. arm32v7, arm64v8)
+docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
+
+# Arm32v7
+docker build -t boinc/client:arm32v7 -f Dockerfile.arm32v7 .
+docker push boinc/client:arm32v7
+
+# Arm64v8
+docker build -t boinc/client:arm64v8 -f Dockerfile.arm64v8 .
+docker push boinc/client:arm64v8
+
 # Remove all the docker images
 docker system prune -a
